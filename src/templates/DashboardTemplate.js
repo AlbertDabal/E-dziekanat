0f4211f2c7b0e6558bb52f2cf86bd106f0078adc
Albert Dąbal <60232394+AlbertDabal@users.noreply.github.com>
2021-04-22 14:48:15 +0200

    add TopBar and delete Main.js and repair DashboardTemplate

--- src/templates/DashboardTemplate.js
+++ src/templates/DashboardTemplate.js
@@ -3,15 +3,35 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Sidebar from 'components/organism/Navigation/Sidebar';
+import TopBar from 'components/organism/TopBar/TopBar';
 
 const Wrapper = styled.div`
   display: flex;
 `;
 
+const MainWrapper = styled.div`
+  width: 81%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  background: #f7f8fc;
+  padding: 2rem;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
 const DashboardTemplate = ({ children }) => (
   <Wrapper>
     <Sidebar />
-    {children}
+    <MainWrapper>
+      <TopBar />
+      <ContentWrapper>{children}</ContentWrapper>
+    </MainWrapper>
   </Wrapper>
 );
 
