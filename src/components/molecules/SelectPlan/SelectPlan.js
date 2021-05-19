492be00d7bcce78934a09a2ef46cb7e7755ebe99
Albert Dąbal <60232394+AlbertDabal@users.noreply.github.com>
2021-05-19 12:43:55 +0200

    fuction NextDay

--- src/components/molecules/SelectPlan/SelectPlan.js
+++ src/components/molecules/SelectPlan/SelectPlan.js
@@ -1,51 +1,45 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Heading from 'components/atoms/Heading/Heading';
-import { SetSelect, SetSelectDefault } from 'api/FetchPlan';
 import { nanoid } from 'nanoid';
+import Button from 'components/atoms/Button/Button';
 
 const Select = styled.select`
   font-size: ${({ theme }) => theme.fontSize.s};
   margin: 0px 30px 0 20px;
+  border-radius: 30px;
+  padding: 5px 10px;
+  outline: none;
 `;
 
 const Wrapper = styled.div`
+  margin-top: 20px;
   margin-left: 10px;
   display: flex;
   margin-bottom: 25px;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 
-function SelectPlan({ TypePlan }) {
-  const [dataSelect, setDataSelect] = useState(null);
-  const [itemSelected, setItemSelected] = useState(null);
+const StyledButton = styled(Button)`
+  height: 40px;
+  cursor: pointer;
+  margin-right: 30px;
+  text-transform: uppercase;
+`;
+
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Right = styled.div``;
+
+function SelectPlan({ TypePlan, dataSelect, FetchSelect, itemSelected, setItemSelected, NextDay }) {
   const [roleSelected, setRoleSelected] = useState(sessionStorage.getItem('Kod_roli'));
-
-  useEffect(() => {
-    FetchSelectDefult();
-  }, []);
-
-  async function FetchSelectDefult() {
-    const res = await SetSelectDefault();
-    try {
-      setItemSelected(res.data.IdDomyslne);
-      setDataSelect(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  async function FetchSelect() {
-    const res = await SetSelect(roleSelected);
-    try {
-      setItemSelected(0);
-      setDataSelect(res.data);
-      TypePlan(roleSelected, itemSelected);
-    } catch (err) {
-      console.log(err);
-    }
-  }
 
   function handleChange(event) {
     setItemSelected(event.target.value);
@@ -54,27 +48,31 @@ function SelectPlan({ TypePlan }) {
 
   function handleChangeRole(event) {
     setRoleSelected(event.target.value);
-    console.log(event.target.value);
-    FetchSelect();
-    TypePlan(event.target.value, itemSelected);
+    FetchSelect(event.target.value);
   }
-  if (dataSelect !== null) {
+  if (dataSelect) {
     return (
       <Wrapper>
-        <Heading>Rola:</Heading>
-        <Select value={roleSelected} onChange={handleChangeRole}>
-          <option value="student">Student</option>
-          <option value="wykladowca">Wykładowca</option>
-        </Select>
-        <Heading>{roleSelected === 'wykladowca' ? 'Wykładowca:' : 'Grupa:'}</Heading>
-        <Select value={itemSelected} onChange={handleChange} defaultValue={dataSelect.IdDomyslne}>
-          {roleSelected === 'wykladowca' ? <option value={0}>-------------</option> : null}
-          {dataSelect.dane.map((item) => (
-            <option value={item.value} key={nanoid()}>
-              {item.visible}
-            </option>
-          ))}
-        </Select>
+        <StyledButton style={{ backgroundColor: '#457B9D' }}>Poprzedni</StyledButton>
+        <Left>
+          <Heading>Rola</Heading>
+          <Select value={roleSelected} onChange={handleChangeRole}>
+            <option value="student">Student</option>
+            <option value="wykladowca">Wykładowca</option>
+          </Select>
+          <Heading>{roleSelected === 'wykladowca' ? 'Wykładowca:' : 'Grupa:'}</Heading>
+          <Select value={itemSelected} onChange={handleChange} defaultValue={dataSelect.IdDomyslne}>
+            {roleSelected === 'wykladowca' ? <option value={0}>-------------</option> : null}
+            {dataSelect.dane.map((item) => (
+              <option value={item.value} key={nanoid()}>
+                {item.visible}
+              </option>
+            ))}
+          </Select>
+        </Left>
+        <Right>
+          <StyledButton onClick={() => NextDay()}>Następny</StyledButton>
+        </Right>
       </Wrapper>
     );
   }
@@ -83,6 +81,11 @@ function SelectPlan({ TypePlan }) {
 
 SelectPlan.propTypes = {
   TypePlan: PropTypes.func.isRequired,
+  setItemSelected: PropTypes.func.isRequired,
+  dataSelect: PropTypes.element.isRequired,
+  itemSelected: PropTypes.element.isRequired,
+  FetchSelect: PropTypes.func.isRequired,
+  NextDay: PropTypes.func.isRequired,
 };
 
 export default SelectPlan;
--- src/views/LessonPlan.js
+++ src/views/LessonPlan.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import DashboardTemplate from 'templates/DashboardTemplate';
 import SelectPlan from 'components/molecules/SelectPlan/SelectPlan';
 import styled from 'styled-components';
-import { SetActualyPlan, SetPlan } from 'api/FetchPlan';
+import { SetActualyPlan, SetPlan, SetSelectDefault, SetSelect } from 'api/FetchPlan';
 import Heading from 'components/atoms/Heading/Heading';
 import { nanoid } from 'nanoid';
 
@@ -22,11 +22,34 @@ const WrapperPlan = styled.div`
 
 const LesssonPlan = () => {
   const [dataPlan, setDataPlan] = useState(null);
+  const [dataSelect, setDataSelect] = useState(null);
+  const [itemSelected, setItemSelected] = useState(null);
   const [date, setDate] = useState(null);
 
   useEffect(() => {
     FetchPlanCurrent();
   }, []);
+
+  async function FetchSelectDefult() {
+    const res = await SetSelectDefault();
+    try {
+      setItemSelected(res.data.IdDomyslne);
+      setDataSelect(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function FetchSelect(role) {
+    const res = await SetSelect(role);
+    try {
+      setItemSelected(0);
+      setDataSelect(res.data);
+      FetchPlan(role, 1);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   async function FetchPlan(kodPlanu, idPola) {
     try {
@@ -45,12 +68,36 @@ const LesssonPlan = () => {
     } catch (err) {
       console.log(err);
     }
+
+    FetchSelectDefult();
+  }
+
+  const SetItem = (item) => {
+    setItemSelected(item);
+  };
+
+  function NextDay() {
+    console.log(date.DataDo.toJSON());
+
+    // const test = Date.parse(date.DataDo) + 7;
+    // const nowa = new Date();
+
+    // const data = date.getDate() + 7;
+
+    // console.log(nowa);
   }
 
   return (
     <DashboardTemplate>
       <Wrapper>
-        <SelectPlan TypePlan={FetchPlan} />
+        <SelectPlan
+          TypePlan={FetchPlan}
+          dataSelect={dataSelect}
+          FetchSelect={FetchSelect}
+          itemSelected={itemSelected}
+          setItemSelected={SetItem}
+          NextDay={NextDay}
+        />
         {dataPlan !== null && date !== null ? (
           <WrapperPlan>
             {dataPlan.Tydzien.map((item) => (
